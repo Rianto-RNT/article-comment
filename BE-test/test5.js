aa25ccbd4c2293cbaad234aa461bdea892b693ea
Rianto <rianto.rnt@gmail.com>
2021-11-15 21:57:49 +0800

    First commit

---
+++ BE-test/test5.js
@@ -0,0 +1,18 @@
+/**
+ * Direction:
+ * Find prefix of the word from array of string
+ *
+ * Expected Result:
+ * fl
+ */
+const words = ["flower", "flow", "flight"];
+
+function result(words) {
+  // Your Code Here
+
+  
+
+  return words;
+}
+
+console.log(result(words));
